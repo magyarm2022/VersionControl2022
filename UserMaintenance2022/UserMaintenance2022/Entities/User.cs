87c4c7fc7ff28bd8c2b70edecd1ade0f212709a0
Magyar Melinda <magyarm@outlook.com>
2022-09-29 10:09:59 +0200

    business logic

--- UserMaintenance2022/UserMaintenance2022/Entities/User.cs
+++ UserMaintenance2022/UserMaintenance2022/Entities/User.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace UserMaintenance2022.Entities
 {
-    class User
+    public class User
     {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
     }
-}
